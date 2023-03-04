8b68a7acb23ce8e16b19747128a5765c828d1a96
amb <mikaelh1651@gmail.com>
2023-03-04 18:15:31 +0900

    feat: 전체 기능 구현

--- bin/dart_baseball.dart
+++ bin/dart_baseball.dart
@@ -1,5 +1,16 @@
-import 'package:dart_baseball/dart_baseball.dart' as dart_baseball;
+import 'package:dart_baseball/controller/baseball_controller.dart';
+import 'package:dart_baseball/screen/home_screen.dart';
+import 'package:dart_baseball/service/baseball_service.dart';
+import 'package:dart_baseball/service/baseball_validation.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_baseball.calculate()}!');
+  final homeScreen = HomeScreen();
+  final baseballService = BaseballService();
+  final baseballValidation = BaseballValidation();
+  final baseballController = BaseballController(
+    homeScreen: homeScreen,
+    baseballService: baseballService,
+    baseballValidation: baseballValidation,
+  );
+  baseballController.play();
 }
--- lib/controller/baseball_controller.dart
+++ lib/controller/baseball_controller.dart
@@ -4,14 +4,49 @@ import 'package:dart_baseball/service/baseball_validation.dart';
 
 class BaseballController {
   final HomeScreen homeScreen;
-  final BaseballService baseBallService;
-  final BaseballValidation baseBallValidation;
+  final BaseballService baseballService;
+  final BaseballValidation baseballValidation;
 
   BaseballController({
     required this.homeScreen,
-    required this.baseBallService,
-    required this.baseBallValidation,
+    required this.baseballService,
+    required this.baseballValidation,
   });
 
-  void play() {}
+  void play() {
+    homeScreen.startView();
+    List<int> createdNumbers = baseballService.createRandomNumber();
+    _gameState(createdNumbers);
+  }
+
+  void _gameState(List<int> createdNumbers) {
+    do {
+      _inGame(createdNumbers);
+    } while (homeScreen.gameEndView() == '1');
+  }
+
+  void _inGame(List<int> createdNumbers) {
+    Map<String, int> ballCount;
+    do {
+      ballCount = baseballService.checkBallCount(
+        createdNumbers,
+        _getInput(),
+      );
+      homeScreen.ballCountView(ballCount);
+    } while (ballCount['strike'] != 3);
+  }
+
+  List<int> _getInput() {
+    String input = '';
+    do {
+      input = homeScreen.getInputView();
+    } while (!_validateInput(input));
+    return input.split('').map((e) => int.parse(e)).toList();
+  }
+
+  bool _validateInput(String input) {
+    return (baseballValidation.isNumber(input) &&
+        baseballValidation.isUnique(input) &&
+        baseballValidation.isThreeCiphers(input));
+  }
 }
