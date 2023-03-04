55df2d6c6494cd18679555f2d540ed4a23d3b114
amb <mikaelh1651@gmail.com>
2023-03-04 16:41:04 +0900

    feat(output_view): 출력 문구 작성

--- lib/component/output_view.dart
+++ lib/component/output_view.dart
@@ -1,1 +1,26 @@
+class OutputView {
+  void showStart() {
+    print('숫자 야구 게임을 시작합니다.\n');
+  }
 
+  void showInputMessage() {
+    print('숫자를 입력해주세요 : ');
+  }
+
+  void showBallCount(Map<String, int> ballCount) {
+    if (ballCount['ball'] == 0) {
+      print('${ballCount['strike']}스트라이크\n');
+    }
+    if (ballCount['strike'] == 0) {
+      print('${ballCount['ball']}볼\n');
+    }
+    if (ballCount['ball'] != 0 && ballCount['strike'] != 0) {
+      print('${ballCount['ball']}볼 ${ballCount['strike']}스트라이크\n');
+    }
+  }
+
+  void showGameEnd() {
+    print('3개의 숫자를 모두 맞히셨습니다! 게임 종료\n');
+    print('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n');
+  }
+}
