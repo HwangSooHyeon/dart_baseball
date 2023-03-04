382726ae9b784bdceca77627b84e968f9a6129d4
amb <mikaelh1651@gmail.com>
2023-03-04 17:04:36 +0900

    fix(home_screen): home_screen 내부 view 관련 class private으로 수정

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,24 +2,24 @@ import 'package:dart_baseball/component/input_view.dart';
 import 'package:dart_baseball/component/output_view.dart';
 
 class HomeScreen {
-  final InputView inputView = InputView();
-  final OutputView outputView = OutputView();
+  final InputView _inputView = InputView();
+  final OutputView _outputView = OutputView();
 
   void startView() {
-    outputView.showStart();
+    _outputView.showStart();
   }
 
   String getInputView() {
-    outputView.showInputMessage();
-    return inputView.readCommandLine();
+    _outputView.showInputMessage();
+    return _inputView.readCommandLine();
   }
 
   void ballCountView(Map<String, int> ballCount) {
-    outputView.showBallCount(ballCount);
+    _outputView.showBallCount(ballCount);
   }
 
   String gameEndView() {
-    outputView.showGameEnd();
-    return inputView.readCommandLine();
+    _outputView.showGameEnd();
+    return _inputView.readCommandLine();
   }
 }
