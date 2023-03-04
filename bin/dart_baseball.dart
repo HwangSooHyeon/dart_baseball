f2c7de1db3c8c394063f51fe0e0b908c50283882
amb <mikaelh1651@gmail.com>
2023-03-04 12:13:14 +0900

    first commit

---
+++ bin/dart_baseball.dart
@@ -0,0 +1,5 @@
+import 'package:dart_baseball/dart_baseball.dart' as dart_baseball;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_baseball.calculate()}!');
+}
---
+++ test/dart_baseball_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_baseball/dart_baseball.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
